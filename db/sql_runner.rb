b817570b66213a25740e33ea55760cc6bb55cda1
Joanna Gora <joanna.gora@protonmail.com>
2018-07-21 16:34:24 +0100

    corrected the name of the database

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -2,7 +2,7 @@ require("pg")
 class SqlRunner
   def self.run sql, values = []
     begin
-      db = PG.connect({ dbname: 'music_collection', host: 'localhost' })
+      db = PG.connect({ dbname: 'horses', host: 'localhost' })
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
