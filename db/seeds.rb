34095fdc7aec16ff0df347a36559c8247eb325c3
Joanna Gora <joanna.gora@protonmail.com>
2018-07-21 16:44:51 +0100

    created save method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 require_relative('../models/horse')
+require_relative('../models/trainer')
 
 horse1 = Horse.new({'name' => 'Magnateria', 'breed' => 'Arabian'})
 horse1.save
+
+trainer1 = Trainer.new({'first_name' => 'Jack', 'last_name' => 'Johnson'})
+trainer1.save
