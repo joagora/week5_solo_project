8aed3c752c87ef83b89901b4a535edcacaf3541a
Joanna Gora <joanna.gora@protonmail.com>
2018-07-21 22:19:15 +0100

    required files for controllers

---
+++ app.rb
@@ -0,0 +1,9 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('controllers/horses_controller.rb')
+require_relative('controllers/trainings_controller.rb')
+require_relative('controllers/trainers_controller.rb')
+
+get '/' do
+  erb( :index )
+end
