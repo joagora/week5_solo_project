40f385febe73d75720516c04d78272352d2f2bc6
Joanna Gora <joanna.gora@protonmail.com>
2018-07-21 16:32:39 +0100

    added day_of_week column to trainings

--- db/horses.sql
+++ db/horses.sql
@@ -21,6 +21,7 @@ CREATE TABLE trainings (
   type VARCHAR(255),
   scheduled_time VARCHAR(255),
   duration INT2,
+  day_of_week VARCHAR(255),
   horse_id INT4 REFERENCES horses(id) ON DELETE CASCADE,
   trainer_id INT4 REFERENCES trainers(id) ON DELETE CASCADE
 );
