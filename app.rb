69c9726d1292dc2119d9096105b72a4844891591
Joanna Gora <joanna.gora@protonmail.com>
2018-07-22 12:25:06 +0100

    added navigation

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-require( 'sinatra' )
-require( 'sinatra/contrib/all' )
+require('sinatra')
+require('sinatra/contrib/all')
 require_relative('controllers/horses_controller.rb')
 require_relative('controllers/trainings_controller.rb')
 require_relative('controllers/trainers_controller.rb')
 
 get '/' do
-  erb( :index )
+  erb :index
 end
