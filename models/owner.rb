ffc75367c7c48b767ff92b904513b6e65bf01bd0
Joanna Gora <joanna.gora@protonmail.com>
2018-07-22 12:52:42 +0100

    created intilize method for owner

---
+++ models/owner.rb
@@ -0,0 +1,9 @@
+require_relative('../db/sql_runner')
+class Owner
+  def initialize options
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+  end
+
+end
