283857add9a517b0b28e1556f13280729b563770
Joanna Gora <joanna.gora@protonmail.com>
2018-07-23 23:23:55 +0100

    moved /owners/validate route above the owners/:id

--- controllers/owners_controller.rb
+++ controllers/owners_controller.rb
@@ -19,6 +19,17 @@ post '/owners' do
   erb :"owners/create"
 end
 
+post '/owners/validate' do
+  username = params['username']
+  if Owner.check_if_exists?(username)
+    erb :"owners/validation_failed"
+  else
+    @owner = Owner.new(params)
+    @owner.save()
+    erb :"owners/create"
+  end
+end
+
 post '/owners/login' do
   username = params['username']
   @found_owner = Owner.filter_by_username(username)
@@ -39,15 +50,4 @@ get '/owners/:id/horses' do
   owner = Owner.find(params['id'])
   @horses_by_owner = owner.horses
   erb :"owners/horses_index"
-end
-
-post '/owners/validate' do
-  username = params['username']
-  if Owner.check_if_exists?(username)
-    erb :"owners/validation_failed"
-  else
-    @owner = Owner.new(params)
-    @owner.save()
-    erb :"owners/create"
-  end
 end
