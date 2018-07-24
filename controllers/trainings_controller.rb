3536392983f8a09d4d431100067ec212a59069f8
Joanna Gora <joanna.gora@protonmail.com>
2018-07-24 15:23:39 +0100

    added trainings/new passing the horse_id in params

--- controllers/trainings_controller.rb
+++ controllers/trainings_controller.rb
@@ -2,3 +2,8 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/training')
 also_reload('./models/*')
+
+get '/trainings/new' do
+  @horse = Horse.find(params['horse_id'])
+  erb :"trainings/new"
+end
