518ee0b8b1ed5b3d03c82c551c8b48871935f8ca
Joanna Gora <joanna.gora@protonmail.com>
2018-07-24 19:58:01 +0100

    removed position absolute from nav

--- controllers/trainings_controller.rb
+++ controllers/trainings_controller.rb
@@ -3,6 +3,13 @@ require('sinatra/contrib/all')
 require_relative('../models/training')
 also_reload('./models/*')
 
+
+get '/trainings' do
+  @trainings = Training.all
+  erb :"trainings/index"
+end
+
+#new
 get '/trainings/new' do
   @horse = Horse.find(params['horse_id'])
   erb :"trainings/new"
