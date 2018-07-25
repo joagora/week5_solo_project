041745692966e3985b8a9a51394d3aa9a87fd283
Joanna Gora <joanna.gora@protonmail.com>
2018-07-25 17:41:32 +0100

    unified markup for buttons

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require_relative('controllers/trainings_controller.rb')
 require_relative('controllers/trainers_controller.rb')
 require_relative('controllers/owners_controller.rb')
 require_relative('controllers/admin_controller.rb')
+
 get '/' do
   erb :index
 end
