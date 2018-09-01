4f4af5d10d5bd5b9e3a80825deec2966fcbd65b4
Joanna Gora <joanna.gora@protonmail.com>
2018-09-01 18:49:12 +0100

    edit procfile

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -2,7 +2,8 @@ require("pg")
 class SqlRunner
   def self.run sql, values = []
     begin
-      db = PG.connect({ dbname: 'horsesdb', host: 'localhost' })
+      db = PG.connect({ dbname: 'dfbb3ov9bc1ari', host: 'ec2-107-22-221-60.compute-1.amazonaws.com',
+      port: 5432, user: 'ytpngekeemrfaq', password: '2b71eb821018f91098e6292ade63919c7d290db446a37c04b61c78789fa4c7eb'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
@@ -11,3 +12,8 @@ class SqlRunner
     return result
   end
 end
+
+
+PG.connect( {dbname: 'dasdkdsaldkj',
+host: 'ec2-1-1-1-1-1.compute-1.amazonaws.com',
+port: 5432, user: 'nsdlkdjalskjd', password: 'sadlskjadlkjASDAD'})
